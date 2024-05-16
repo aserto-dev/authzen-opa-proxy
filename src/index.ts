f355dff89d6132df11652ce6bb2171b8785e10d4
Omri Gazitt <ogazitt@gmail.com>
2024-05-15 18:47:00 -0700

    updated opa authorizer to match new policy and added error handling

--- src/index.ts
+++ src/index.ts
@@ -19,22 +19,15 @@ const directory = new Directory()
 const AUTHORIZER_SERVICE_URL = process.env.AUTHORIZER_SERVICE_URL ?? 'http://localhost:8181'
 const PORT = process.env.PORT ?? 8080
 
-const pathMappings: Record<string, string> = {
-  can_read_user: 'todoApp.GET.users.__userID',
-  can_read_todos: 'todoApp.GET.todos',
-  can_create_todo: 'todoApp.POST.todos',
-  can_update_todo: 'todoApp.PUT.todos.__id',
-  can_delete_todo: 'todoApp.DELETE.todos.__id',
-}
-
 async function handler(req: JWTRequest, res: Response) {
   const request: AuthZenRequest = req.body
   const identity = request.subject?.identity
   const properties = await directory.getUserByIdentity(identity)
-  const policyPath = pathMappings[request?.action?.name]
+  const actionName = request.action?.name
   const resource = request.resource
+  const context = request.context
   let decision = false
-  if (identity && policyPath) {
+  if (identity && actionName) {
     try {
       const authorizerUrl = `${AUTHORIZER_SERVICE_URL}/v1/query`
       log(`Authorizer: ${authorizerUrl}`)
@@ -42,23 +35,22 @@ async function handler(req: JWTRequest, res: Response) {
         'content-type': 'application/json',
       }
       const data = {
-        query: `x = data.${policyPath}.allowed`,
+        query: `x = data.todoApp.${actionName}.allowed`,
         input: {
           user: {
-            id: properties.id,
-            key: properties.id,
+            id: properties?.id,
             properties,
           },
           resource,
-          context: {},
+          context,
         },
       }
-      log(data)
+      log(JSON.stringify(data, null, 2))
       const response = await axios.post(authorizerUrl, data, { headers })
-      log(response?.data)
+      log(JSON.stringify(response?.data, null, 2))
       const result =
         response?.data?.result && response?.data?.result.length && response?.data?.result[0]
-      decision = result['x']
+      decision = (result && result['x']) ?? false
     } catch (e) {
       console.error(e)
       res.status(403).send()
